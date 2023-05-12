0c58d3f6dbec9c0b7b80ec119b8d3a81f71a8199
dhioclaudee <s2200505@student.unklab.ac.id>
2023-05-12 15:51:12 +0800

    insert page Akun & Home

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,8 @@ import SignIn from './SignIn'
 import Pesanan from './Pesanan'
 import Splash from './Splash'
 import Signup from './Signup'
+import Akun from './Akun'
+import Home from './Home'
 
 
-export{SignIn,Pesanan,Splash,Signup}
+export{SignIn,Pesanan,Splash,Signup,Akun,Home}
